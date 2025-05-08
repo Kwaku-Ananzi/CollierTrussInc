bdf49c74dbf8eef2b855a522bd5a5ae67ef7cba0
JohnC <jkcherrelus@gmail.com>
2025-05-08 18:27:46 -0400

    email changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,34 @@
 import { Routes } from '@angular/router';
 import { HeroComponent } from './components/hero/hero.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 export const routes: Routes = [
      {
           path: 'home',
+          title: 'home',
           component: HeroComponent
+     },
+     {
+          path: 'about-us',
+          title: 'aboutUs',
+          component: AboutUsComponent
+     },
+     {
+          path:'contact-us',
+          title: 'contactUs',
+          component: ContactUsComponent
      },
      {
           path: '',
           redirectTo: '/home',
           pathMatch: 'full'
      } // Default Route 
+     ,
+     {
+          path: '**',
+          redirectTo: '/home',
+          pathMatch: 'full'
+     }
+
 ];
--- src/app/components/contact-us/contact-us.component.ts
+++ src/app/components/contact-us/contact-us.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Subject } from 'rxjs';
+import { FormGroup,FormControl, ReactiveFormsModule } from '@angular/forms';
+/*
+import MimeNode from 'nodemailer/lib/mime-node';
+import nodemailer from 'nodemailer';
+import { getLogger } from 'nodemailer/lib/shared';
+import { error } from 'console';
+*/
 
 @Component({
   selector: 'app-contact-us',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact-us.component.html',
   styleUrl: './contact-us.component.scss'
 })
+
+
 export class ContactUsComponent {
+
+  
+
+  contactForm = new FormGroup({
+  fullName: new FormControl(''),
+  mobile_no: new FormControl(''),
+  email_id: new FormControl(''),
+  subject: new FormControl(''),
+  comment: new FormControl(''),
+  attach_file: new FormControl('')
+});
+
+
+onSubmit():void{
+  
+}
+
+/*
+export const sendEmail = async(from:string,to:string,subject:string,html:string,attachment:File) => {
+  const transport = nodemailer.createTransport({
+    service: process.env['MAIL_HOST'],
+    auth:{
+      user: process.env['MAIL_USERNAME'],
+      pass: process.env['MAIL_PASSWORD']
+    }
+  })
+  **/
+/*
+  const mailOptions = {
+    from: from,
+    to: toString,
+    subject: subject,
+    html:html,
+    attachment:attachment
+  }
+
+*/
+
+  
 
 }
